690de6b7b3d01329e136cec905a43e93f24f4c3f
Rushi9238 <patilrushikesh8683@gmail.com>
2023-02-09 23:37:40 +0530

    09 feb 2023 solve this problem

---
+++ LC1528_Shuffle_String.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class LC1528_Shuffle_String {
+    public static void main(String[] args) {
+        // given output
+        String s="codeleet";
+        int arr[]={4,5,6,7,0,2,1,3};
+        String temp="";
+        for(int i=0;i<arr.length;i++){
+                for(int j=0;j<arr.length;j++){
+                    if(i==arr[j]){
+                        temp=temp+s.charAt(j);
+                    }
+                }
+        }
+        System.out.println(temp);
+    }
+}
